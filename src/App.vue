6cc5375ad1235b0a22378d543f5c369e928f2796
zaheralmattrud <zaheralmattrud@gmail.com>
2024-02-25 22:19:55 +0200

    fix AppLayout format use command 'npm run lint -- --fix'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <router-view />
+  <AppLayout />
 </template>
+
+<script>
+import AppLayout from "./components/AppLayout.vue";
+
+export default {
+  components: {
+    AppLayout,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/AppLayout.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="layout">
+    <v-layout>
+      <v-app-bar></v-app-bar>
+    </v-layout>
+  </div>
+</template>
