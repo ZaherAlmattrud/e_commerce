b0ac130390aeed56254506639e3b2b4b6915b8e4
zaheralmattrud <zaheralmattrud@gmail.com>
2024-02-25 21:36:58 +0200

    import and use vuetify , @mdi/font

--- src/main.js
+++ src/main.js
@@ -3,4 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+// vuetify
+
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(store).use(vuetify).use(router).mount("#app");
